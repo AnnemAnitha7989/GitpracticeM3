0b784fd1a15870c8025d784e00f2b9d07922da1d
anith <anith@Anitha_Lenovo>
2024-06-17 11:25:21 +0530

    First commit

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void Test1()
+	{
+		Reporter.log("Test1-sampleTest",true);
+	}
+
+}
---
+++ src/test/java/tests/Test2.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void Test1()
+	{
+		Reporter.log("Test2-sampleTest",true);
+	}
+
+}
